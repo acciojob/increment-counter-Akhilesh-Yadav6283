e8311259e8cdc963902665fcd50c5ee83a716b41
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2023-11-25 00:40:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const counterElement = document.getElementById('counter');
+const incrementBtn = document.getElementById('incrementBtn');
+
+let counterValue=0;
+counterElement.textContent=counterValue;
+
+function buttonFunction(){
+ alert(counterValue) ;
+	counterValue++;
+counterElement.textContent=counterValue;	
+}
+  // incrementBtn.addEventListener('click', handleIncrement);
